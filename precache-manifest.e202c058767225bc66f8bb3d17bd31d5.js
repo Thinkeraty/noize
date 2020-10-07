9037810682d60d0617564a7dab82843647cc0c08
Samyak Jain <contact.thinkeraty@gmail.com>
2020-10-07 15:13:34 +0530

    Updates

---
+++ precache-manifest.e202c058767225bc66f8bb3d17bd31d5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4c126fa1ee23d436df425772ba5be3",
+    "url": "/noize/index.html"
+  },
+  {
+    "revision": "69f9fbc407e737ef617f",
+    "url": "/noize/static/css/main.3e440cd4.chunk.css"
+  },
+  {
+    "revision": "874768b8c053f32a33e6",
+    "url": "/noize/static/js/2.2451eb87.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/noize/static/js/2.2451eb87.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69f9fbc407e737ef617f",
+    "url": "/noize/static/js/main.7a494a37.chunk.js"
+  },
+  {
+    "revision": "90062cf4d9e6426381cd",
+    "url": "/noize/static/js/runtime-main.5c487a47.js"
+  },
+  {
+    "revision": "a34828488ed9c1ca8430ccdf8de1a109",
+    "url": "/noize/static/media/i1.a3482848.gif"
+  },
+  {
+    "revision": "fb884beddaf0c94f8957a184c2cd8a2e",
+    "url": "/noize/static/media/i1.fb884bed.mp4"
+  },
+  {
+    "revision": "07b5865dc081f07d7f44d48b0db25d42",
+    "url": "/noize/static/media/i10.07b5865d.gif"
+  },
+  {
+    "revision": "d437e37cf3bd8421d37fea27dfb236cd",
+    "url": "/noize/static/media/i10.d437e37c.mp4"
+  },
+  {
+    "revision": "10d6ff2dc283dd8c68b4f7f99d272b68",
+    "url": "/noize/static/media/i2.10d6ff2d.gif"
+  },
+  {
+    "revision": "dc5a16667c4ae9eaabf4e27017669832",
+    "url": "/noize/static/media/i2.dc5a1666.mp4"
+  },
+  {
+    "revision": "261131ce268974eb7aa9961bd026e3a6",
+    "url": "/noize/static/media/i3.261131ce.gif"
+  },
+  {
+    "revision": "fba6ba9498b41142f338b1ab07c018f2",
+    "url": "/noize/static/media/i3.fba6ba94.mp4"
+  },
+  {
+    "revision": "3d787ba848c3018582262d22846936dc",
+    "url": "/noize/static/media/i4.3d787ba8.mp4"
+  },
+  {
+    "revision": "ef722ea4ebf562247c226416252acdc7",
+    "url": "/noize/static/media/i4.ef722ea4.gif"
+  },
+  {
+    "revision": "56ba89adbba540c41437f4f8d9a6fe2b",
+    "url": "/noize/static/media/i5.56ba89ad.mp4"
+  },
+  {
+    "revision": "72946a3d17cf96c3ba52d97ba6479032",
+    "url": "/noize/static/media/i5.72946a3d.gif"
+  },
+  {
+    "revision": "2d01199788b9a2d11cd9a2f59950ffe3",
+    "url": "/noize/static/media/i6.2d011997.gif"
+  },
+  {
+    "revision": "596dcf99026491ad3a1e6814fc72714d",
+    "url": "/noize/static/media/i6.596dcf99.mp4"
+  },
+  {
+    "revision": "208bc9a0aa952b9abcf76e8be57953bf",
+    "url": "/noize/static/media/i7.208bc9a0.gif"
+  },
+  {
+    "revision": "e289e414c4c5d6815313febcfb31a569",
+    "url": "/noize/static/media/i7.e289e414.mp4"
+  },
+  {
+    "revision": "4e1932de53dfa0a5ec989f6e992c4141",
+    "url": "/noize/static/media/i8.4e1932de.mp4"
+  },
+  {
+    "revision": "fea37f6fa68d9ebe66ba7000321bb890",
+    "url": "/noize/static/media/i8.fea37f6f.gif"
+  },
+  {
+    "revision": "6f4438c3197d55bab332dd4c3fd85370",
+    "url": "/noize/static/media/i9.6f4438c3.gif"
+  },
+  {
+    "revision": "af8d73553ec6d4ae4ef44aaaa182ec25",
+    "url": "/noize/static/media/i9.af8d7355.mp4"
+  }
+]);
